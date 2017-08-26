743756b446654aa98a0d77aa1f51357936e26592
Manuel Avalos <manuel.avalos@globant.com>
2017-08-26 19:19:37 -0300

    Sim Fuego v0.0.4 - ahora el fuego se consume y deja cenizas

--- simFuego2.js
+++ simFuego2.js
@@ -13,65 +13,70 @@ $(document).ready(function(){
 	var images = [];
 	var game_loop;
 	var terrainMap = [
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,3,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
-		[0,0,0,0,0,7,7,7,1,0,0,3,0,1],
-		[0,0,0,0,7,0,7,0,0,0,0,3,0,0],
-		[0,0,0,0,7,0,7,0,0,0,0,3,0,0],
-		[0,0,0,0,7,0,7,0,0,0,0,0,0,0],
-		[0,0,0,0,7,7,7,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,8,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,1,7],
+		[7,7,7,1,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,4,4,4,7,7,7,7,7],
+		[7,7,7,7,7,7,4,7,4,7,7,7,7,7],
+		[7,7,7,7,7,7,4,7,4,7,7,7,7,7],
+		[7,7,7,7,7,7,4,7,4,7,7,7,7,7],
+		[7,7,7,7,7,7,4,4,4,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+		[7,7,7,7,7,7,7,7,7,7,1,7,7,7]
 	];
 
 	/*OBJETOS*/
 	var objetos = [];
 	//Arena
-	objetos[0] = {id:'arena', posArray:0, fuel:0, sx:64, sy:32, sw:32, sh:32};
+	objetos[0] = {id:'', category:'arena', posArray:0, fuel:0, sx:64, sy:32, sw:32, sh:32};
 
 	//Fuego
-	objetos[1] = {id:'fuego', posArray:1, fuel:0, burnPower:100, sx:448, sy:480, sw:32, sh:32};
+	objetos[1] = {id:'', category:'fuego', posArray:1, fuel:100, burnPower:100, sx:448, sy:480, sw:32, sh:32};
 
 	//Tierra
-	objetos[2] = {id:'tierra', posArray:2, fuel:0, sx:64, sy:0, sw:32, sh:32};
+	objetos[2] = {id:'', category:'tierra', posArray:2, fuel:0, sx:64, sy:0, sw:32, sh:32};
 
 	//Suelo cemento blando
-	objetos[3] = {id:'suelo_cemento_blando', posArray:3, fuel:0, sx:0, sy:0, sw:32, sh:32};
+	objetos[3] = {id:'', category:'suelo_cemento_blando', posArray:3, fuel:0, sx:0, sy:0, sw:32, sh:32};
 
 	//Suelo cemento duro
-	objetos[4] = {id:'suelo_cemento_duro', posArray:4, fuel:0, sx:32, sy:0, sw:32, sh:32};
+	objetos[4] = {id:'', category:'suelo_cemento_duro', posArray:4, fuel:0, sx:32, sy:0, sw:32, sh:32};
 
 	//Diamante
-	objetos[5] = {id:'diamante', posArray:5, fuel:0, sx:64, sy:96, sw:32, sh:32};
+	objetos[5] = {id:'', category:'diamante', posArray:5, fuel:0, sx:64, sy:96, sw:32, sh:32};
 
 	//Agua
-	objetos[6] = {id:'agua', posArray:6, fuel:-10, sx:448, sy:416, sw:32, sh:32};
+	objetos[6] = {id:'', category:'agua', posArray:6, fuel:-10, sx:448, sy:416, sw:32, sh:32};
 
 	//Madera
-	objetos[7] = {id:'madera', posArray:7, fuel:100, sx:160, sy:32, sw:32, sh:32};
+	objetos[7] = {id:'', category:'madera', posArray:7, fuel:100, sx:160, sy:32, sw:32, sh:32};
 
 	//Ceniza
-	objetos[8] = {id:'ceniza', posArray:8, fuel:0, sx:160, sy:64, sw:32, sh:32};
+	objetos[8] = {id:'', category:'ceniza', posArray:8, fuel:0, sx:160, sy:64, sw:32, sh:32};
 
 	// Inicializador
 	var init = function	init()	{
 		preloadimages(['img/terrain.png']).done(function(img){
 			images = img;
+			mapingTerrainFromNumbersToObjects();
 			initTerrain();
 		});
 		
+	};
+
+	var mapingTerrainFromNumbersToObjects = function mapingTerrainFromNumbersToObjects(){
 		for (var fila = 0; fila < terrainMap.length ; fila++) {
 			for (var col = 0; col <= terrainMap[fila].length -1; col++) {
+				objetos[terrainMap[fila][col]].id = 'col' + col + 'fila' + fila;
 				//Agrego cada objeto en la casilla que le corresponde.
 				terrainMap[fila][col] = objetos[terrainMap[fila][col]];
 			}
 		}
-	};
+	}
 
 	var draw_terrain = function draw_terrain () {
 		ctx.strokeStyle = "black";
@@ -104,60 +109,72 @@ $(document).ready(function(){
 	}
 
 	var fire_propagation = function fire_propagation () {
-		var nextTerrainMap = new Array();
+		var nextTerrainMap = cleanNextTerrainMap(new Array());
 		filaLeng = terrainMap[0].length;
-		colLeng = terrainMap.length;
-
-		for (var fila = 0; fila < terrainMap.length ; fila++) {
-			nextTerrainMap[fila] = new Array();
-			for (var col = 0; col < terrainMap[fila].length; col++) {
-				nextTerrainMap[fila][col] = 0;
-			}
-		}
 
 		for (var fila = 0; fila < terrainMap.length ; fila++) {
 			for (var col = 0; col < terrainMap[fila].length; col++) {
-				var objetoPrincipal = terrainMap[fila][col];
-				if (terrainMap[fila][col].id == 'fuego') {// Si estoy en una casilla con fuego:
+				var fuego = terrainMap[fila][col];
+
+				var leftObject = false;
+				var rightObject = false;
+				var topObject = false;
+				var downObject = false;
+				
+				leftObject = (col - 1 >= 0) ? terrainMap[fila][col - 1] : false;
+				rightObject = (col + 1 >= 0 && rightObject != undefined) ? terrainMap[fila][col + 1] : false;
+				
+				topObject = (fila - 1 >= 0) ? terrainMap[fila - 1][col] : false;
+				downObject = (fila + 1 >= 0 && fila + 1 < filaLeng) ? terrainMap[fila + 1][col] : false;
+
+				if (fuego.category == 'fuego') {// Si estoy en una casilla con fuego:
+					//console.log('['+fila+'-'+col+'] id: ' + fuego.id);
 					// LEFT
-					if (col - 1 >= 0) {
-						if (terrainMap[fila][col - 1].id != 'fuego') {// El material no es fuego?
-							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila][col - 1])) {
+					if (leftObject) {
+						if (leftObject.id != 'fuego') {// El material no es fuego?
+							if (checkIteractionWithFire(fuego, leftObject)) {
 								nextTerrainMap[fila][col - 1] = objetos[1]; //pinto fuego es esa casilla
 							}
 						}
 					};
 
 					//RIGTH
-					if (col + 1 >= 0 && terrainMap[fila][col + 1] != undefined) {
-						if (terrainMap[fila][col + 1].id != 'fuego') {
-							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila][col + 1])) {
+					if (rightObject) {
+						console.log("rightObject: " + topObject.id);
+						if (rightObject.id != 'fuego') {// El material no es fuego?
+							if (checkIteractionWithFire(fuego, rightObject)) {
 								nextTerrainMap[fila][col + 1] = objetos[1]; //pinto fuego es esa casilla
 							}
 						}
 					};
 
 					// TOP
-					if (fila - 1 >= 0) {
-						if (terrainMap[fila - 1][col].id != 'fuego') {
-							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila - 1][col])) {
+					if (topObject) {
+						if (topObject.id != 'fuego') {// El material no es fuego?
+							if (checkIteractionWithFire(fuego, topObject)) {
 								nextTerrainMap[fila - 1][col] = objetos[1]; //pinto fuego es esa casilla
 							}
 						}
 					};
 
 					// BOTTOM
-					if (fila + 1 >= 0 && fila + 1 < filaLeng) {
-						if (terrainMap[fila + 1][col].id != 'fuego') {
-							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila + 1][col])) {
+					if (downObject) {
+						if (downObject.id != 'fuego') {// El material no es fuego?
+							if (checkIteractionWithFire(fuego, downObject)) {
 								nextTerrainMap[fila + 1][col] = objetos[1]; //pinto fuego es esa casilla
 							}
 						}
 					}
+
+					fuego.fuel = fuego.fuel - 10;
+					if (fuego.fuel <= 0) {
+						fuego = objetos[8]; // Si se acabó el fuego, lo pinto como ceniza
+						nextTerrainMap[fila][col] = fuego;
+					}
 				}
 			}
 		};
-
+		console.log("--------------------");
 		for (var fila = 0; fila < nextTerrainMap.length ; fila++) {
 			for (var col = 0; col < nextTerrainMap[fila].length; col++) {
 				if (nextTerrainMap[fila][col] != 0) {
@@ -178,6 +195,18 @@ $(document).ready(function(){
 		}
 		return hasBurn;
 	}
+
+	var cleanNextTerrainMap = function cleanNextTerrainMap(nextTerrainMap) {
+		for (var fila = 0; fila < terrainMap.length ; fila++) {
+			nextTerrainMap[fila] = new Array();
+			for (var col = 0; col < terrainMap[fila].length; col++) {
+				nextTerrainMap[fila][col] = 0;
+			}
+		}
+
+		return nextTerrainMap;
+	}
+
 	var initTerrain = function initTerrain () {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		draw_terrain();
