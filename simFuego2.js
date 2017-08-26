31104af090807387dd4aa97008e823f79e7f6da5
Manuel Avalos <manuel.avalos@globant.com>
2017-08-26 18:12:59 -0300

    simFuego 0.0.3 - Ahora el fuego interactua con los elementos que lo rodean, no quema todo, solo lo que puede ser quemado y dependiendo de la cantidad de fuel que el elemento tenga

--- simFuego2.js
+++ simFuego2.js
@@ -13,18 +13,18 @@ $(document).ready(function(){
 	var images = [];
 	var game_loop;
 	var terrainMap = [
-		[0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,2,0,0,0,0,0,0,0,0,0,0,0],
 		[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,2,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,2,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,2,0,0],
-		[0,0,0,0,0,2,0,0,0,0,0,2,0,0],
-		[0,0,0,0,2,0,2,0,0,0,0,2,0,0],
-		[0,0,0,0,2,0,2,0,0,0,0,2,0,0],
-		[0,0,0,0,2,0,2,0,0,0,0,0,0,0],
-		[0,0,0,0,0,2,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+		[0,0,3,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+		[0,0,0,0,0,7,7,7,1,0,0,3,0,1],
+		[0,0,0,0,7,0,7,0,0,0,0,3,0,0],
+		[0,0,0,0,7,0,7,0,0,0,0,3,0,0],
+		[0,0,0,0,7,0,7,0,0,0,0,0,0,0],
+		[0,0,0,0,7,7,7,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,8,0,0,0,0,0,0],
 		[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 		[0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	];
@@ -32,23 +32,45 @@ $(document).ready(function(){
 	/*OBJETOS*/
 	var objetos = [];
 	//Arena
-	objetos[0] = {sx:64, sy:32, sw:32, sh:32};
+	objetos[0] = {id:'arena', posArray:0, fuel:0, sx:64, sy:32, sw:32, sh:32};
 
-	//Lava
-	objetos[1] = {sx:448, sy:480, sw:32, sh:32};
+	//Fuego
+	objetos[1] = {id:'fuego', posArray:1, fuel:0, burnPower:100, sx:448, sy:480, sw:32, sh:32};
 
-	//Lava2
-	objetos[8] = {sx:128, sy:480, sw:32, sh:32};
+	//Tierra
+	objetos[2] = {id:'tierra', posArray:2, fuel:0, sx:64, sy:0, sw:32, sh:32};
 
-	//Lava
-	objetos[2] = {sx:0, sy:0, sw:32, sh:32};
+	//Suelo cemento blando
+	objetos[3] = {id:'suelo_cemento_blando', posArray:3, fuel:0, sx:0, sy:0, sw:32, sh:32};
+
+	//Suelo cemento duro
+	objetos[4] = {id:'suelo_cemento_duro', posArray:4, fuel:0, sx:32, sy:0, sw:32, sh:32};
+
+	//Diamante
+	objetos[5] = {id:'diamante', posArray:5, fuel:0, sx:64, sy:96, sw:32, sh:32};
+
+	//Agua
+	objetos[6] = {id:'agua', posArray:6, fuel:-10, sx:448, sy:416, sw:32, sh:32};
+
+	//Madera
+	objetos[7] = {id:'madera', posArray:7, fuel:100, sx:160, sy:32, sw:32, sh:32};
+
+	//Ceniza
+	objetos[8] = {id:'ceniza', posArray:8, fuel:0, sx:160, sy:64, sw:32, sh:32};
 
 	// Inicializador
 	var init = function	init()	{
 		preloadimages(['img/terrain.png']).done(function(img){
 			images = img;
-			dibujar();
+			initTerrain();
 		});
+		
+		for (var fila = 0; fila < terrainMap.length ; fila++) {
+			for (var col = 0; col <= terrainMap[fila].length -1; col++) {
+				//Agrego cada objeto en la casilla que le corresponde.
+				terrainMap[fila][col] = objetos[terrainMap[fila][col]];
+			}
+		}
 	};
 
 	var draw_terrain = function draw_terrain () {
@@ -56,10 +78,12 @@ $(document).ready(function(){
 		ctx.strokeRect(0, 0, 450, 450);
 		for (var fila = 0; fila < terrainMap.length ; fila++) {
 			for (var col = 0; col <= terrainMap[fila].length -1; col++) {
-				sx = objetos[terrainMap[fila][col]].sx;
-				sy = objetos[terrainMap[fila][col]].sy;
-				sw = objetos[terrainMap[fila][col]].sw;
-				sh = objetos[terrainMap[fila][col]].sh;
+				//Seteo las variables para poder dibujar en el tablero
+				var obj = terrainMap[fila][col];
+				sx = objetos[obj.posArray].sx;
+				sy = objetos[obj.posArray].sy;
+				sw = objetos[obj.posArray].sw;
+				sh = objetos[obj.posArray].sh;
 				ix  = 32 * col;
 				iy  = 32 * fila;
 				iw  = 32;
@@ -93,32 +117,41 @@ $(document).ready(function(){
 
 		for (var fila = 0; fila < terrainMap.length ; fila++) {
 			for (var col = 0; col < terrainMap[fila].length; col++) {
-				if (terrainMap[fila][col] == 1) {// Si estoy en una casilla con fuego:
+				var objetoPrincipal = terrainMap[fila][col];
+				if (terrainMap[fila][col].id == 'fuego') {// Si estoy en una casilla con fuego:
 					// LEFT
 					if (col - 1 >= 0) {
-						if (terrainMap[fila][col - 1] == 0) {// El material no es fuego?
-							nextTerrainMap[fila][col - 1] = 1; //pinto fuego es esa casilla
+						if (terrainMap[fila][col - 1].id != 'fuego') {// El material no es fuego?
+							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila][col - 1])) {
+								nextTerrainMap[fila][col - 1] = objetos[1]; //pinto fuego es esa casilla
+							}
 						}
 					};
 
 					//RIGTH
-					if (col + 1 >= 0) {
-						if (terrainMap[fila][col + 1] == 0) {
-							nextTerrainMap[fila][col + 1] = 1;
+					if (col + 1 >= 0 && terrainMap[fila][col + 1] != undefined) {
+						if (terrainMap[fila][col + 1].id != 'fuego') {
+							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila][col + 1])) {
+								nextTerrainMap[fila][col + 1] = objetos[1]; //pinto fuego es esa casilla
+							}
 						}
 					};
 
 					// TOP
 					if (fila - 1 >= 0) {
-						if (terrainMap[fila - 1][col] == 0) {
-							nextTerrainMap[fila - 1][col] = 1;
+						if (terrainMap[fila - 1][col].id != 'fuego') {
+							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila - 1][col])) {
+								nextTerrainMap[fila - 1][col] = objetos[1]; //pinto fuego es esa casilla
+							}
 						}
 					};
 
 					// BOTTOM
 					if (fila + 1 >= 0 && fila + 1 < filaLeng) {
-						if (terrainMap[fila + 1][col] == 0) {
-							nextTerrainMap[fila + 1][col] = 1;
+						if (terrainMap[fila + 1][col].id != 'fuego') {
+							if (checkIteractionWithFire(objetoPrincipal, terrainMap[fila + 1][col])) {
+								nextTerrainMap[fila + 1][col] = objetos[1]; //pinto fuego es esa casilla
+							}
 						}
 					}
 				}
@@ -134,7 +167,18 @@ $(document).ready(function(){
 		}
 	}
 
-	var dibujar = function dibujar () {
+	var checkIteractionWithFire = function checkIteractionWithFire(fire, objetoSecundario) {
+		var hasBurn = false;
+		if (objetoSecundario.fuel > 0) {
+			objetoSecundario.fuel - fire.burnPower;
+			if (objetoSecundario.fuel < 0) {
+				objetoSecundario.fuel = 0;
+			}
+			hasBurn = true;
+		}
+		return hasBurn;
+	}
+	var initTerrain = function initTerrain () {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		draw_terrain();
 	}
